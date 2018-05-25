6000bef1ab3ab065cedd2dc0f3a15689e808cde0
Manoj Kumar Patra <patra.manoj0@gmail.com>
2018-05-25 17:07:44 +0530

    Create new file for String Builder example

--- Patterns/Builder/Builder.java
+++ Patterns/Builder/Builder.java
@@ -84,38 +84,9 @@ class HtmlElement {
 class DemoBuilder {
 
     public static void main(String[] args) {
-//        String hello = "hello";
-//
-//        System.out.println("<p>" + hello + "</p>");
-//
-//        String[] words = {"hello", "world"};
-//
-//        StringBuilder sb = new StringBuilder();
-//
-//        sb.append("<ul>\n");
-//        for (String word : words) {
-//            sb.append(String.format("  <li>%s</li>\n", word));
-//        }
-//        sb.append("</ul>");
-//
-//        System.out.println(sb);
-
         HtmlBuilder builder = new HtmlBuilder("ul");
         builder.addChild("li", "hello");
         builder.addChild("li", "world");
         System.out.println(builder);
     }
 }
-
-/**
- *  OUTPUT:
- *
- *  <ul>
- *   <li>
- *     hello
- *   </li>
- *   <li>
- *     world
- *   </li>
- *  </ul>
- */
---
+++ Patterns/Builder/StringBuilderExample.java
@@ -0,0 +1,34 @@
+package Patterns.Builder;
+
+class DemoStringBuilder {
+    public static void main(String[] args) {
+        String hello = "hello";
+
+        System.out.println("<p>" + hello + "</p>");
+
+        String[] words = {"hello", "world"};
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<ul>\n");
+        for (String word : words) {
+            sb.append(String.format("  <li>%s</li>\n", word));
+        }
+        sb.append("</ul>");
+
+        System.out.println(sb);
+    }
+}
+
+/**
+ *  OUTPUT of string builder example:
+ *
+ *  <ul>
+ *   <li>
+ *     hello
+ *   </li>
+ *   <li>
+ *     world
+ *   </li>
+ *  </ul>
+ */
