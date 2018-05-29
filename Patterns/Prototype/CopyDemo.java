07c442abf675f20fcc4a6e1592ac9d9232fdc4c6
Manoj Kumar Patra <patra.manoj0@gmail.com>
2018-05-29 16:14:09 +0530

    Example to understand difference between deep copy and shallow copy

---
+++ Patterns/Prototype/CopyDemo.java
@@ -0,0 +1,77 @@
+package Patterns.Prototype;
+
+import java.util.Arrays;
+
+class Address implements Cloneable {
+    public String streetName;
+    public int houseNumber;
+
+    public Address(String streetName, int houseNumber) {
+        this.streetName = streetName;
+        this.houseNumber = houseNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetName='" + streetName + '\'' +
+                ", houseNumber=" + houseNumber +
+                '}';
+    }
+
+    //    Default function of clone() is to perform a
+    //    shallow copy instead of a deep copy
+
+    //    It is better to use other mechanisms for implementing
+    //    deep copy instead of Cloneable interface
+
+    //    DEEP COPY
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return new Address(streetName, houseNumber);
+    }
+}
+
+class Person implements Cloneable{
+    public String [] names;
+    public Address address;
+
+    public Person(String [] names, Address address) {
+        this.names = names;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "names=" + Arrays.toString(names) +
+                ", address=" + address +
+                '}';
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return new Person(names.clone(), (Address) address.clone());
+    }
+}
+
+class Demo {
+    public static void main(String[] args)
+        throws Exception {
+        Person john = new Person(new String[]{"John", "Smith"},
+                new Address("London Road", 123));
+
+//        jane and john both refer to the same object
+//        SHALLOW COPY
+//        Person jane = john;
+//        jane.names[0] = "Jane";
+//        jane.address.houseNumber = 124;
+
+        Person jane = (Person) john.clone();
+        jane.names[0] = "Jane";
+        jane.address.houseNumber = 124;
+
+        System.out.println(john);
+        System.out.println(jane);
+    }
+}
